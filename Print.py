fa20ef739e5a86fa4b77dc59e71a86456c3c05c3
karimgraa12-tech <karimgraa12@gmail.com>
2025-10-15 16:11:37 +0200

    Add files via upload

---
+++ Print.py
@@ -0,0 +1,30 @@
+import time
+print("hello")
+
+time.sleep(5)
+print("i lost my phone")
+
+time.sleep(5)
+print("help me to find it pls")
+
+print("u have 10 sec")
+
+for i in range (10, -1, -1):
+    print(i)
+    for _ in range(50_000_000):
+        pass
+
+
+print("you still hasn't found it?")
+time.sleep(5)
+
+print("well ur DEAD")
+
+time.sleep(5)
+print("Program is shutting down in...")
+
+for i in range (5, -1 -1):
+    print(i)
+    for _ in range(50_000_000):
+        pass
+    time.sleep(1)
